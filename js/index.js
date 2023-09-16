ad26b9673a87e725d03f23ab6e3cbd1f2dd4c4c5
kauadt <kauadt2000@gmail.com>
2023-09-16 11:37:23 -0300

    Update

--- js/index.js
+++ js/index.js
@@ -11,3 +11,42 @@ function changeTab(event) {
         behavior: 'smooth'
     });
 }
+
+
+const doubts = [
+    { question: 'Qual é a duração do curso?', answer: '5 anos' },
+    { question: 'Qual é a média salarial?', answer: 'R$ 8.479,39' },
+    { question: 'Qual é a duração do curso?', answer: '5 anos' },
+    { question: 'Qual é a média salarial?', answer: 'R$ 6.815,10' },
+    { question: 'Qual é a duração do curso?', answer: '5 anos' },
+    { question: 'Qual é a média salarial?', answer: 'R$ 7.899,19' },
+    { question: 'Qual é a duração do curso?', answer: '5 anos' },
+    { question: 'Qual é a média salarial?', answer: 'R$ 7.671,00' },
+    { question: 'Qual é a duração do curso?', answer: '5 anos' },
+    { question: 'Qual é a média salarial?', answer: 'R$ 8.231,78' },
+];
+
+function changeContent(el) {
+    el.classList.remove('doubtsCircle');
+    el.classList.add('doubtsSquare');
+    let key = el.getAttribute('key');
+    el.textContent = doubts[key].answer;
+}
+
+function resetContent(el) {
+    el.classList.remove('doubtsSquare');
+    el.classList.add('doubtsCircle');
+    let key = el.getAttribute('key');
+    el.textContent = doubts[key].question;
+}
+
+
+/*
+
+   
+    
+
+
+    
+
+*/
